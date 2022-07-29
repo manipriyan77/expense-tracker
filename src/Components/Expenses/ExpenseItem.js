1dba5bd48ca7e72b25fc077fc25586d91fc4af10
Manipriyan <manipriyan@6storage.com>
2022-07-29 16:47:13 +0530

    finished expense form

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  // let title=props.title;
+  const [title,setTitle]=useState(props.title)
+
+  const changeTitleHandler =()=>{
+    setTitle("Updated")
+    console.log(title)
+  }
   return (
     <Card className="expense-item" key={props.id}>
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={()=>changeTitleHandler()}>Change title</button>
     </Card>
   );
 }
